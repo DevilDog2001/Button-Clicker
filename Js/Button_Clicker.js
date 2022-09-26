6bd2422ce63613769600e1847651a89248c16cc7
Richardson Antoine <91235784+DevilDog2001@users.noreply.github.com>
2022-09-26 19:47:33 -0400

    Add files via upload

---
+++ Js/Button_Clicker.js
@@ -0,0 +1,11 @@
+function removePTag(){
+    var x = document.getElementById("btn4");
+    x.remove();
+}
+function logout(){
+    var y = document.getElementById("btn")
+    y.innerText = "Logout"
+}
+function alerted(){
+    alert("ninja was liked")
+}
